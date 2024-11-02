2c02c563f2ee9a667fdce34ce448e142616243e3
xieyufeng <347667675@qq.com>
2024-11-02 16:00:03 +0800

    First Commit

--- src/main/java/com/fred/tool/geo/GeoUtil.java
+++ src/main/java/com/fred/tool/geo/GeoUtil.java
@@ -21,24 +21,27 @@ public class GeoUtil {
         List<Poi> allList = new ArrayList<>();
 
         //拉取全部数据
-        for (int i = 1; i <100; i++) {
+        for (int i = 1; i < 100; i++) {
             JSONObject page = GetOnePage(region, String.valueOf(i));
-            log.info("page:{}",page);
-            if(page.getInteger("count")<25){
+            log.info("page:{}", page);
+            if (page == null ) {
                 break;
             }
             JSONArray pois = page.getJSONArray("pois");
             List<Poi> poiList = pois.toJavaList(Poi.class);
             allList.addAll(poiList);
+            if(page.getInteger("count") < 25){
+                break;
+            }
         }
-        allList.forEach( item -> {
+        allList.forEach(item -> {
             log.info(JSONObject.toJSONString(item));
         });
         return allList;
     }
 
 
-    public static JSONObject GetOnePage(String region,String pageNum){
+    public static JSONObject GetOnePage(String region, String pageNum) {
         OkHttpClient client = new OkHttpClient();
 
         HttpUrl.Builder urlBuilder = HttpUrl.parse("https://restapi.amap.com/v5/place/text").newBuilder();
@@ -53,15 +56,16 @@ public class GeoUtil {
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
-            if (!response.isSuccessful()){
+            if (!response.isSuccessful()) {
                 throw new IOException("Unexpected code " + response);
             }
             // 打印响应内容
             String resStr = response.body().string();
             JSONObject res = JSON.parseObject(resStr);
-            if (res.get("status").toString().equals("1")){
+            if (res.get("status").toString().equals("1")) {
                 return res;
-            }else {
+            } else {
+                log.error(resStr);
                 return null;
             }
         } catch (IOException e) {
